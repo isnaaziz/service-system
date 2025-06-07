21615c50dbf1e8ae17e9a4187374aa08854afabd
azizzz <aziznurohman28@gmail.com>
2025-06-07 12:03:20 +0700

    [patch] router.go add cors origin

--- router/router.go
+++ router/router.go
@@ -1,13 +1,28 @@
 package router
 
 import (
+	"os"
 	"service_system/controllers"
+	"strings"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	// Ambil origins dari .env
+	origins := os.Getenv("CORS_ORIGINS")
+	allowedOrigins := strings.Split(origins, ",")
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     allowedOrigins,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	// Auth group
 	auth := r.Group("/")
